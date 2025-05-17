7930e227073ddf60ff59d77b8ed8a1dad267d514
gyanendra-rathore <tinnyrathore9@gmail.com>
2025-05-17 15:12:03 +0530

    Second Commit

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let box = document.querySelector(".box")
 
+let value = 0
+box.addEventListener('click', function(){
+  if(value==0) {
+    box.style.backgroundColor = 'red'
+  value = 1;}
+  else { 
+    box.style.backgroundColor = 'grey'
+  value = 0;
+  }
+})
